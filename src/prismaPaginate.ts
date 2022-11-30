69f57891e85f631e6b43f88312c1dc75a84dae2e
sandrewtx08 <sandrewtx08@protonmail.com>
2022-11-30 18:15:01 -0300

    feat: callback and pagination

--- src/prismaPaginate.ts
+++ src/prismaPaginate.ts
@@ -7,19 +7,59 @@ function prismaPaginate<
   },
   Query extends Parameters<Table["findMany"]>[0],
   Result extends ReturnType<Table["findMany"]>
->(table: Table, query: Query, pagination?: { page: number; limit: number }) {
+>(
+  table: Table,
+  query: Query,
+  pagination: { page: number; limit: number },
+  callback?: (result: Result) => void
+): void;
+function prismaPaginate<
+  Table extends {
+    findMany(...args: any[]): PrismaPromise<any>;
+    count(...args: any[]): PrismaPromise<number>;
+  },
+  Query extends Parameters<Table["findMany"]>[0],
+  Result extends ReturnType<Table["findMany"]>
+>(table: Table, query: Query, callback: (result: Result) => void): void;
+function prismaPaginate<
+  Table extends {
+    findMany(...args: any[]): PrismaPromise<any>;
+    count(...args: any[]): PrismaPromise<number>;
+  },
+  Query extends Parameters<Table["findMany"]>[0],
+  Result extends ReturnType<Table["findMany"]>
+>(
+  table: Table,
+  query: Query,
+  pagination?: { page: number; limit: number }
+): Promise<Result>;
+function prismaPaginate<
+  Table extends {
+    findMany(...args: any[]): PrismaPromise<any>;
+    count(...args: any[]): PrismaPromise<number>;
+  },
+  Query extends Parameters<Table["findMany"]>[0],
+  Result extends ReturnType<Table["findMany"]>
+>(
+  table: Table,
+  query: Query,
+  paginationOrCallback?:
+    | { page: number; limit: number }
+    | ((result: Result) => void),
+  callback?: (result: Result) => void
+) {
   return new Promise<Result>((resolve, rejects) => {
     table.count(query).then((count) => {
       query =
-        pagination?.limit || pagination?.page
+        paginationOrCallback?.limit || paginationOrCallback?.page
           ? {
               ...query,
-              take: pagination.limit,
-              skip: pagination.page * pagination?.limit,
+              take: paginationOrCallback.limit,
+              skip: paginationOrCallback.page * paginationOrCallback?.limit,
             }
           : query;
 
-      if (pagination) {
+      if (paginationOrCallback) {
         if (query.skip > count) rejects("Pagination exceed the total of rows");
       }
 
