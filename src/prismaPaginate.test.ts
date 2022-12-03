43df6b07b1b650c16b9b8c9478ba8a70956e26b3
sandrewtx08 <sandrewtx08@protonmail.com>
2022-12-03 07:02:47 -0300

    chore: test return

--- src/prismaPaginate.test.ts
+++ src/prismaPaginate.test.ts
@@ -1,15 +1,38 @@
-import { Model } from "./prismaPaginate";
+import prismaPaginate, { Model } from "./prismaPaginate";
 import { jest } from "@jest/globals";
 
 describe("return value", () => {
+  const data = [{ id: 1 }, { id: 2 }, { id: 3 }] as const;
+  type Data = Partial<typeof data[0]>;
+
   const model = {
-    findMany: jest.fn(),
-    count: jest.fn(),
-  } as unknown as Model;
+    findMany: jest.fn(async ({ where }: { where: Data }) =>
+      data.find(({ id }) => where.id == id)
+    ),
+    count: jest.fn(async () => data.length),
+  } as unknown as Model<Data, { where: Data }>;
 
-  it("callback", () => {});
+  it("callback", () => {
+    prismaPaginate(model, { where: { id: 1 } }, (error, result) => {
+      expect(error).toBe(null);
+      expect(result).toBe(data[0]);
+    });
+  });
 
-  it("callbackWithoutPagination", () => {});
+  it("callbackWithoutPagination", () => {
+    prismaPaginate(model, { where: { id: 1 } }, (error, result) => {
+      expect(error).toBe(null);
+      expect(result).toBe(data[0]);
+    });
+  });
 
-  it("paginationWithoutCallback", () => {});
+  it("paginationWithoutCallback", () => {
+    expect(
+      prismaPaginate(model, { where: { id: 1 } }, { page: 1, limit: 1 })
+    ).toBeInstanceOf(Promise);
+  });
+
+  it("withoutPagination", () => {
+    expect(prismaPaginate(model, { where: { id: 1 } })).toBeInstanceOf(Promise);
+  });
 });
