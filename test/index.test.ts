e9fb9291d7c01580331a5a77f243440ca442bf44
sandrewtx08 <sandrewtx08@protonmail.com>
2022-12-03 20:15:04 -0300

    chore(test): pagination return

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,45 +1,113 @@
 import { prismaPaginate } from "../src";
-import { data, model } from "./utils";
+import { db, model } from "./utils";
 
-describe("return value", () => {
-  const [id_1] = data;
-
-  describe("callback", () => {
-    it("withoutPagination", () => {
-      prismaPaginate(model, { where: { id: 1 } }, (error, result) => {
-        expect(error).toBe(null);
-        expect(result?.result).toBe(id_1);
-      });
-    });
-
-    it("withPagination", () => {
-      prismaPaginate(
-        model,
-        { where: { id: 1 } },
-        { page: 1, limit: 1 },
-        (error, result) => {
-          expect(error).toBe(null);
-          expect(result?.result).toBe(id_1);
-          expect(result?.count).toBe(data.length);
-        }
-      );
+describe("callback", () => {
+  it("withoutPagination", () => {
+    prismaPaginate(model, {}, (error, result) => {
+      expect(error).toBe(null);
+      expect(result).not.toHaveProperty([
+        "count",
+        "hasNextPage",
+        "hasPrevPage",
+        "limit",
+        "page",
+        "totalPages",
+      ]);
     });
   });
 
-  describe("promise", () => {
-    it("withoutPagination", () => {
-      prismaPaginate(model, { where: { id: 1 } }).then((result) => {
-        expect(result?.result).toBe(id_1);
-      });
+  it("withPagination page == 0", () => {
+    prismaPaginate(model, {}, { page: 0, limit: 1 }, (error, result) => {
+      expect(error).toBe(null);
+      expect(result?.count).toBe(db.length);
+      expect(result?.hasNextPage).toBe(true);
+      expect(result?.hasPrevPage).toBe(false);
+      expect(result?.limit).toBe(1);
+      expect(result?.page).toBe(0);
+      expect(result?.totalPages).toBe(db.length);
     });
+  });
 
-    it("withoutPagination", () => {
-      prismaPaginate(model, { where: { id: 1 } }, { page: 1, limit: 1 }).then(
-        (result) => {
-          expect(result?.result).toBe(id_1);
-          expect(result?.count).toBe(data.length);
-        }
-      );
+  it("withPagination index != 1", () => {
+    prismaPaginate(model, {}, { page: 1, limit: 1 }, (error, result) => {
+      expect(error).toBe(null);
+      expect(result?.count).toBe(db.length);
+      expect(result?.hasNextPage).toBe(true);
+      expect(result?.hasPrevPage).toBe(true);
+      expect(result?.limit).toBe(1);
+      expect(result?.page).toBe(1);
+      expect(result?.totalPages).toBe(db.length);
+    });
+  });
+
+  it("withPagination page > totalPage", () => {
+    prismaPaginate(model, {}, { page: 3, limit: 1 }, (error, result) => {
+      expect(error).toBe(null);
+      expect(result?.count).toBe(db.length);
+      expect(result?.hasNextPage).toBe(false);
+      expect(result?.hasPrevPage).toBe(true);
+      expect(result?.limit).toBe(1);
+      expect(result?.page).toBe(3);
+      expect(result?.totalPages).toBe(db.length);
+    });
+  });
+});
+
+describe("promise", () => {
+  it("withoutPagination", () => {
+    prismaPaginate(model, {}).then((result) => {
+      expect(result).not.toHaveProperty([
+        "count",
+        "hasNextPage",
+        "hasPrevPage",
+        "limit",
+        "page",
+        "totalPages",
+      ]);
+    });
+  });
+
+  it("withoutPagination", () => {
+    prismaPaginate(model, {}, { page: 0, limit: 1 }).then((result) => {
+      expect(result?.count).toBe(db.length);
+      expect(result?.hasNextPage).toBe(true);
+      expect(result?.hasPrevPage).toBe(false);
+      expect(result?.limit).toBe(1);
+      expect(result?.page).toBe(0);
+      expect(result?.totalPages).toBe(db.length);
+    });
+  });
+
+  it("withPagination page == 0", () => {
+    prismaPaginate(model, {}, { page: 0, limit: 1 }).then((result) => {
+      expect(result?.count).toBe(db.length);
+      expect(result?.hasNextPage).toBe(true);
+      expect(result?.hasPrevPage).toBe(false);
+      expect(result?.limit).toBe(1);
+      expect(result?.page).toBe(0);
+      expect(result?.totalPages).toBe(db.length);
+    });
+  });
+
+  it("withPagination index != 1", () => {
+    prismaPaginate(model, {}, { page: 1, limit: 1 }).then((result) => {
+      expect(result?.count).toBe(db.length);
+      expect(result?.hasNextPage).toBe(true);
+      expect(result?.hasPrevPage).toBe(true);
+      expect(result?.limit).toBe(1);
+      expect(result?.page).toBe(1);
+      expect(result?.totalPages).toBe(db.length);
+    });
+  });
+
+  it("withPagination page > totalPage", () => {
+    prismaPaginate(model, {}, { page: 3, limit: 1 }).then((result) => {
+      expect(result?.count).toBe(db.length);
+      expect(result?.hasNextPage).toBe(false);
+      expect(result?.hasPrevPage).toBe(true);
+      expect(result?.limit).toBe(1);
+      expect(result?.page).toBe(3);
+      expect(result?.totalPages).toBe(db.length);
     });
   });
 });
--- test/utils.ts
+++ test/utils.ts
@@ -1,9 +1,9 @@
 import { Model } from "../src/model";
 
-const data = [{ id: 1 }, { id: 2 }, { id: 3 }] as const;
+const db = [{ id: 1 }, { id: 2 }, { id: 3 }] as const;
 const model = {
-  findMany: async ({ where }: any) => data.find(({ id }) => where.id == id),
-  count: async () => data.length,
+  findMany: async (..._: any[]) => db,
+  count: async (..._: any[]) => db.length,
 } as unknown as Model;
 
-export { data, model };
+export { db, model };
