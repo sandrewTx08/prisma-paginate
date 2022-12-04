f35c67922629455c3dcf28c6a149278e36d4627b
sandrewtx08 <sandrewtx08@protonmail.com>
2022-12-04 20:49:02 -0300

    chore(test): test prisma model

--- test/index.test.ts
+++ test/index.test.ts
@@ -9,12 +9,47 @@ describe("prisma", () => {
   beforeAll((done) => {
     model.createMany({ data: [{ id: 1 }, { id: 2 }, { id: 3 }] }).then(done);
   });
-});
 
-describe("callback", () => {
   it("without pagination", () => {
+    paginate(model, {}, (error, result) => {
+      expect(error).toBe(null);
+      expect(result).toBeInstanceOf(Array);
+      expect(result).not.toHaveProperty([
+        "count",
+        "hasNextPage",
+        "hasPrevPage",
+        "limit",
+        "page",
+        "totalPages",
+      ]);
+    });
+
     paginate(mockModel, {}, (error, result) => {
       expect(error).toBe(null);
+      expect(result).toBeInstanceOf(Array);
+      expect(result).not.toHaveProperty([
+        "count",
+        "hasNextPage",
+        "hasPrevPage",
+        "limit",
+        "page",
+        "totalPages",
+      ]);
+    });
+
+    paginate(model, {}).then((result) => {
+      expect(result).toBeInstanceOf(Array);
+      expect(result).not.toHaveProperty([
+        "count",
+        "hasNextPage",
+        "hasPrevPage",
+        "limit",
+        "page",
+        "totalPages",
+      ]);
+    });
+
+    paginate(mockModel, {}).then((result) => {
       expect(result).toBeInstanceOf(Array);
       expect(result).not.toHaveProperty([
         "count",
@@ -38,12 +73,32 @@ describe("callback", () => {
       expect(result?.page).toBe(1);
       expect(result?.totalPages).toBe(mockDatabase.length);
     });
+
+    paginate(mockModel, {}, { page: 0, limit: 1 }).then((result) => {
+      expect(result.result).toBeDefined();
+      expect(result?.count).toBe(mockDatabase.length);
+      expect(result?.hasNextPage).toBe(true);
+      expect(result?.hasPrevPage).toBe(false);
+      expect(result?.limit).toBe(1);
+      expect(result?.page).toBe(1);
+      expect(result?.totalPages).toBe(mockDatabase.length);
+    });
   });
 
   it("page == 1", () => {
     paginate(mockModel, {}, { page: 1, limit: 1 }, (error, result) => {
       expect(error).toBe(null);
       expect(result).toBeDefined();
+      expect(result?.count).toBe(mockDatabase.length);
+      expect(result?.hasNextPage).toBe(true);
+      expect(result?.hasPrevPage).toBe(false);
+      expect(result?.limit).toBe(1);
+      expect(result?.page).toBe(1);
+      expect(result?.totalPages).toBe(mockDatabase.length);
+    });
+
+    paginate(mockModel, {}, { page: 1, limit: 1 }).then((result) => {
+      expect(result.result).toBeDefined();
       expect(result?.count).toBe(mockDatabase.length);
       expect(result?.hasNextPage).toBe(true);
       expect(result?.hasPrevPage).toBe(false);
@@ -64,11 +119,31 @@ describe("callback", () => {
       expect(result?.page).toBe(2);
       expect(result?.totalPages).toBe(mockDatabase.length);
     });
+
+    paginate(mockModel, {}, { page: 2, limit: 1 }).then((result) => {
+      expect(result.result).toBeDefined();
+      expect(result?.count).toBe(mockDatabase.length);
+      expect(result?.hasNextPage).toBe(true);
+      expect(result?.hasPrevPage).toBe(true);
+      expect(result?.limit).toBe(1);
+      expect(result?.page).toBe(2);
+      expect(result?.totalPages).toBe(mockDatabase.length);
+    });
   });
 
   it("page == totalPage", () => {
     paginate(mockModel, {}, { page: 3, limit: 1 }, (error, result) => {
       expect(error).toBe(null);
+      expect(result?.count).toBe(mockDatabase.length);
+      expect(result?.hasNextPage).toBe(false);
+      expect(result?.hasPrevPage).toBe(true);
+      expect(result?.limit).toBe(1);
+      expect(result?.page).toBe(3);
+      expect(result?.totalPages).toBe(mockDatabase.length);
+    });
+
+    paginate(mockModel, {}, { page: 3, limit: 1 }).then((result) => {
+      expect(result.result).toBeDefined();
       expect(result?.count).toBe(mockDatabase.length);
       expect(result?.hasNextPage).toBe(false);
       expect(result?.hasPrevPage).toBe(true);
@@ -84,70 +159,6 @@ describe("callback", () => {
   //     expect(result).toBe(undefined);
   //   });
   // });
-});
-
-describe("promise", () => {
-  it("without pagination", () => {
-    paginate(mockModel, {}).then((result) => {
-      expect(result).toBeInstanceOf(Array);
-      expect(result).not.toHaveProperty([
-        "count",
-        "hasNextPage",
-        "hasPrevPage",
-        "limit",
-        "page",
-        "totalPages",
-      ]);
-    });
-  });
-
-  it("page == 0", () => {
-    paginate(mockModel, {}, { page: 0, limit: 1 }).then((result) => {
-      expect(result.result).toBeDefined();
-      expect(result?.count).toBe(mockDatabase.length);
-      expect(result?.hasNextPage).toBe(true);
-      expect(result?.hasPrevPage).toBe(false);
-      expect(result?.limit).toBe(1);
-      expect(result?.page).toBe(1);
-      expect(result?.totalPages).toBe(mockDatabase.length);
-    });
-  });
-
-  it("page == 1", () => {
-    paginate(mockModel, {}, { page: 1, limit: 1 }).then((result) => {
-      expect(result.result).toBeDefined();
-      expect(result?.count).toBe(mockDatabase.length);
-      expect(result?.hasNextPage).toBe(true);
-      expect(result?.hasPrevPage).toBe(false);
-      expect(result?.limit).toBe(1);
-      expect(result?.page).toBe(1);
-      expect(result?.totalPages).toBe(mockDatabase.length);
-    });
-  });
-
-  it("index == 2", () => {
-    paginate(mockModel, {}, { page: 2, limit: 1 }).then((result) => {
-      expect(result.result).toBeDefined();
-      expect(result?.count).toBe(mockDatabase.length);
-      expect(result?.hasNextPage).toBe(true);
-      expect(result?.hasPrevPage).toBe(true);
-      expect(result?.limit).toBe(1);
-      expect(result?.page).toBe(2);
-      expect(result?.totalPages).toBe(mockDatabase.length);
-    });
-  });
-
-  it("page == totalPage", () => {
-    paginate(mockModel, {}, { page: 3, limit: 1 }).then((result) => {
-      expect(result.result).toBeDefined();
-      expect(result?.count).toBe(mockDatabase.length);
-      expect(result?.hasNextPage).toBe(false);
-      expect(result?.hasPrevPage).toBe(true);
-      expect(result?.limit).toBe(1);
-      expect(result?.page).toBe(3);
-      expect(result?.totalPages).toBe(mockDatabase.length);
-    });
-  });
 
   // it("page > totalPage", () => {
   //   paginate(model, {}, { page: 4, limit: 1 }).then(
