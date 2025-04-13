e0de436ec8bc4065a155edd59aa782880a1527c0
sandrewtx08 <sandrewtx08@protonmail.com>
2025-04-13 16:33:36 -0300

    refactor: tests to async

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -2,8 +2,8 @@ import { PrismaClient } from "@prisma/client";
 import {
 	ExceedCount,
 	ExceedTotalPages,
-	Pagination,
 	extension,
+	Pagination,
 	PaginationResult,
 } from "../src";
 import { createRandomArray } from "./utils";
@@ -26,162 +26,135 @@ describe("extension", () => {
 		await prisma.$disconnect();
 	});
 
-	it("ExceedCount", (done) => {
-		Promise.all([
+	it("ExceedCount", async () => {
+		await Promise.all([
 			expect(
-				xprisma.model
-					.paginate({
-						limit: randomIds.length + 1,
-						page: 1,
-						exceedCount: true,
-					})
-					.then((result) => {
-						expect(result.exceedCount).toBe(true);
-						expect(result.totalPages).toBe(false);
-						return result;
-					}),
+				xprisma.model.paginate({
+					limit: randomIds.length + 1,
+					page: 1,
+					exceedCount: true,
+				}),
 			).rejects.toThrow(ExceedCount),
 
 			expect(
-				xprisma.model
-					.paginate({
-						limit: randomIds.length - 2,
-						page: 1,
-						exceedCount: true,
-					})
-					.then((result) => {
-						expect(result.exceedCount).toBe(true);
-						expect(result.exceedTotalPages).toBe(false);
-						return result;
-					}),
-			).resolves.toBeTruthy(),
-		]).finally(done);
+				xprisma.model.paginate({
+					limit: randomIds.length - 2,
+					page: 1,
+					exceedCount: true,
+				}),
+			).resolves.toMatchObject({
+				exceedCount: true,
+				exceedTotalPages: false,
+			}),
+		]);
 	});
 
-	it("ExceedTotalPages", (done) => {
-		Promise.all([
+	it("ExceedTotalPages", async () => {
+		await Promise.all([
 			expect(
-				xprisma.model
-					.paginate({
-						limit: 1,
-						page: randomIds.length + 1,
-						exceedTotalPages: true,
-					})
-					.then((result) => {
-						expect(result.exceedCount).toBe(true);
-						expect(result.exceedTotalPages).toBe(false);
-						return result;
-					}),
+				xprisma.model.paginate({
+					limit: 1,
+					page: randomIds.length + 1,
+					exceedTotalPages: true,
+				}),
 			).rejects.toThrow(ExceedTotalPages),
 
 			expect(
-				xprisma.model
-					.paginate({
-						limit: 1,
-						page: randomIds.length - 2,
-						exceedTotalPages: true,
-					})
-					.then((result) => {
-						expect(result.exceedCount).toBe(false);
-						expect(result.exceedTotalPages).toBe(true);
-						return result;
-					}),
-			).resolves.toBeTruthy(),
-		]).finally(done);
-	});
-
-	it("page == 0", (done) => {
-		xprisma.model
-			.paginate({ limit: 1, page: 0 })
-			.then((result) => {
-				expect(result.result).toStrictEqual([randomIds.at(0)]);
-				expect(result.count).toBe(randomIds.length);
-				expect(result.hasNextPage).toBe(true);
-				expect(result.hasPrevPage).toBe(false);
-				expect(result.limit).toBe(1);
-				expect(result.page).toBe(1);
-				expect(result.totalPages).toBe(randomIds.length);
-			})
-			.finally(done);
-	});
-
-	it("page == 1", (done) => {
-		xprisma.model
-			.paginate({}, { limit: 1, page: 1 })
-			.then((result) => {
-				expect(result.result).toStrictEqual([randomIds.at(0)]);
-				expect(result.count).toBe(randomIds.length);
-				expect(result.hasNextPage).toBe(true);
-				expect(result.hasPrevPage).toBe(false);
-				expect(result.limit).toBe(1);
-				expect(result.page).toBe(1);
-				expect(result.totalPages).toBe(randomIds.length);
-			})
-			.finally(done);
-	});
-
-	it("page == totalPages", (done) => {
-		xprisma.model
-			.paginate({ limit: 1, page: randomIds.length })
-			.then((result) => {
-				expect(result.result).toStrictEqual([randomIds.at(-1)]);
-				expect(result.count).toBe(randomIds.length);
-				expect(result.hasNextPage).toBe(false);
-				expect(result.hasPrevPage).toBe(true);
-				expect(result.limit).toBe(1);
-				expect(result.page).toBe(randomIds.length);
-				expect(result.totalPages).toBe(randomIds.length);
-			})
-			.finally(done);
-	});
-
-	it("page == totalPages + 1", (done) => {
-		xprisma.model
-			.paginate({}, { limit: 1, page: randomIds.length + 1 })
-			.then((result) => {
-				expect(result.result).toStrictEqual([]);
-				expect(result.count).toBe(randomIds.length);
-				expect(result.hasNextPage).toBe(false);
-				expect(result.hasPrevPage).toBe(true);
-				expect(result.limit).toBe(1);
-				expect(result.page).toBe(randomIds.length + 1);
-				expect(result.totalPages).toBe(randomIds.length);
-			})
-			.finally(done);
-	});
-
-	it("page == totalPages + 2", (done) => {
-		xprisma.model
-			.paginate({}, { limit: 1, page: randomIds.length + 2 })
-			.then((result) => {
-				expect(result.result).toStrictEqual([]);
-				expect(result.count).toBe(randomIds.length);
-				expect(result.hasNextPage).toBe(false);
-				expect(result.hasPrevPage).toBe(false);
-				expect(result.limit).toBe(1);
-				expect(result.page).toBe(randomIds.length + 2);
-				expect(result.totalPages).toBe(randomIds.length);
-			})
-			.finally(done);
-	});
-
-	it("toJSON", (done) => {
-		xprisma.model
-			.paginate({}, { limit: 1, page: 1 })
-			.then((result) => {
-				expect(result.toJSON()).toStrictEqual({
-					count: randomIds.length,
-					exceedCount: false,
-					exceedTotalPages: false,
-					hasNextPage: true,
-					hasPrevPage: false,
+				xprisma.model.paginate({
 					limit: 1,
-					page: 1,
-					result: [{ id: 0 }],
-					totalPages: randomIds.length,
-				});
-			})
-			.finally(done);
+					page: randomIds.length - 2,
+					exceedTotalPages: true,
+				}),
+			).resolves.toMatchObject({
+				exceedCount: false,
+				exceedTotalPages: true,
+			}),
+		]);
+	});
+
+	it("page == 0", async () => {
+		const result = await xprisma.model.paginate({ limit: 1, page: 0 });
+
+		expect(result.result).toStrictEqual([randomIds.at(0)]);
+		expect(result.count).toBe(randomIds.length);
+		expect(result.hasNextPage).toBe(true);
+		expect(result.hasPrevPage).toBe(false);
+		expect(result.limit).toBe(1);
+		expect(result.page).toBe(1);
+		expect(result.totalPages).toBe(randomIds.length);
+	});
+
+	it("page == 1", async () => {
+		const result = await xprisma.model.paginate({}, { limit: 1, page: 1 });
+
+		expect(result.result).toStrictEqual([randomIds.at(0)]);
+		expect(result.count).toBe(randomIds.length);
+		expect(result.hasNextPage).toBe(true);
+		expect(result.hasPrevPage).toBe(false);
+		expect(result.limit).toBe(1);
+		expect(result.page).toBe(1);
+		expect(result.totalPages).toBe(randomIds.length);
+	});
+
+	it("page == totalPages", async () => {
+		const result = await xprisma.model.paginate({
+			limit: 1,
+			page: randomIds.length,
+		});
+
+		expect(result.result).toStrictEqual([randomIds.at(-1)]);
+		expect(result.count).toBe(randomIds.length);
+		expect(result.hasNextPage).toBe(false);
+		expect(result.hasPrevPage).toBe(true);
+		expect(result.limit).toBe(1);
+		expect(result.page).toBe(randomIds.length);
+		expect(result.totalPages).toBe(randomIds.length);
+	});
+
+	it("page == totalPages + 1", async () => {
+		const result = await xprisma.model.paginate(
+			{},
+			{ limit: 1, page: randomIds.length + 1 },
+		);
+
+		expect(result.result).toStrictEqual([]);
+		expect(result.count).toBe(randomIds.length);
+		expect(result.hasNextPage).toBe(false);
+		expect(result.hasPrevPage).toBe(true);
+		expect(result.limit).toBe(1);
+		expect(result.page).toBe(randomIds.length + 1);
+		expect(result.totalPages).toBe(randomIds.length);
+	});
+
+	it("page == totalPages + 2", async () => {
+		const result = await xprisma.model.paginate(
+			{},
+			{ limit: 1, page: randomIds.length + 2 },
+		);
+
+		expect(result.result).toStrictEqual([]);
+		expect(result.count).toBe(randomIds.length);
+		expect(result.hasNextPage).toBe(false);
+		expect(result.hasPrevPage).toBe(false);
+		expect(result.limit).toBe(1);
+		expect(result.page).toBe(randomIds.length + 2);
+		expect(result.totalPages).toBe(randomIds.length);
+	});
+
+	it("toJSON", async () => {
+		const result = await xprisma.model.paginate({}, { limit: 1, page: 1 });
+
+		expect(result.toJSON()).toStrictEqual({
+			count: randomIds.length,
+			exceedCount: false,
+			exceedTotalPages: false,
+			hasNextPage: true,
+			hasPrevPage: false,
+			limit: 1,
+			page: 1,
+			result: [{ id: 0 }],
+			totalPages: randomIds.length,
+		});
 	});
 
 	it("offset", async () => {
@@ -222,28 +195,42 @@ describe("extension", () => {
 		await prisma.model3.deleteMany();
 	});
 
-	it("nextPage", (done) => {
-		xprisma.model.paginate({}, { limit: randomIds.length / 2 }).then((result) => {
-			expect(result.result.at(0)).toStrictEqual({ id: 0 });
-			expect(result.count).toBe(randomIds.length);
-			expect(result.hasNextPage).toBe(true);
-			expect(result.hasPrevPage).toBe(false);
-			expect(result.limit).toBe(randomIds.length / 2);
-			expect(result.page).toBe(1);
-			expect(result.totalPages).toBe(2);
+	it("nextPage", async () => {
+		const firstResult = await xprisma.model.paginate(
+			{},
+			{ limit: randomIds.length / 2 },
+		);
 
-			result
-				.nextPage()
-				.then((result) => {
-					expect(result.result.at(0)).toStrictEqual({ id: 50 });
-					expect(result.count).toBe(randomIds.length);
-					expect(result.hasNextPage).toBe(false);
-					expect(result.hasPrevPage).toBe(true);
-					expect(result.limit).toBe(randomIds.length / 2);
-					expect(result.page).toBe(2);
-					expect(result.totalPages).toBe(2);
-				})
-				.finally(done);
-		});
+		expect(firstResult.result.at(0)).toStrictEqual({ id: 0 });
+		expect(firstResult.count).toBe(randomIds.length);
+		expect(firstResult.hasNextPage).toBe(true);
+		expect(firstResult.hasPrevPage).toBe(false);
+		expect(firstResult.limit).toBe(randomIds.length / 2);
+		expect(firstResult.page).toBe(1);
+		expect(firstResult.totalPages).toBe(2);
+
+		const secondResult = await firstResult.nextPage();
+
+		expect(secondResult.result.at(0)).toStrictEqual({ id: 50 });
+		expect(secondResult.count).toBe(randomIds.length);
+		expect(secondResult.hasNextPage).toBe(false);
+		expect(secondResult.hasPrevPage).toBe(true);
+		expect(secondResult.limit).toBe(randomIds.length / 2);
+		expect(secondResult.page).toBe(2);
+		expect(secondResult.totalPages).toBe(2);
+	});
+
+	it("model.count & model.findMany + orderBy", async () => {
+		expect(
+			xprisma.model.paginate({
+				limit: 100,
+				orderBy: {
+					id: "asc",
+					Model2: {
+						_count: "desc",
+					},
+				},
+			}),
+		).rejects.toThrow(Error);
 	});
 });
